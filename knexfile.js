668ffedcbfd51ddf44c85d446b19d42cb89ee2ca
Dan Corman <dan.corman@gmail.com>
2015-06-18 15:04:12 -0500

    backend routing up, oAuth up, db up

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: 'ec2-23-23-188-252.compute-1.amazonaws.com',
-      database: 'd7dub96b5jqr4l'
+      database: 'learn_dev'
     },
     pool: {
       min: 2,
@@ -15,6 +14,21 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
+
+  test: {
+    client: 'postgresql',
+    connection: {
+      database: 'learn_test'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
   production: {
     client: 'postgresql',
     connection: 'postgres://yuofkehmpkcdbk:D15Qec1JRfhWn_vE7-CIbsbo28@ec2-54-83-43-118.compute-1.amazonaws.com:5432/d8h2gnum0his57',
