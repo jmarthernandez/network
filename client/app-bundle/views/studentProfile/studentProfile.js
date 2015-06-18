14501d9ff36735c8f882b46adb3aee8b9bdce162
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-18 12:14:32 -0500

    Routing set up and views moved to views folder closed #14

--- client/app-bundle/views/studentProfile/studentProfile.js
+++ client/app-bundle/views/studentProfile/studentProfile.js
@@ -1,29 +1,64 @@
-var m             	= require('mithril');
-var Navbar        	= require('../../components/navbar/navbar.js');
-var NewApp        	= require('../../components/forms/newApp/newApp.js');
-var Messaging     	= require('../../components/messaging/messaging.js');
-var OnsiteInterview = require('../../components/forms/onsiteInterview/onsiteInterview.js');
-var Phases        	= require('../../components/phases/phases.js');
-var PhoneScreen     = require('../../components/forms/phoneScreen/phoneScreen.js');
-var StudentInfo   	= require('../../components/studentInfo/studentInfo.js');
-var StudentJobs     = require('../../components/studentJobs/studentJobs.js')
-
-//Models
-var StudentApps = require('../../models/studentApps.model.js')
+var m = require('mithril')
 
 exports.controller = function () {
-  StudentApps.fetchApps();
-  StudentApps.fetchInfo();
+    var ctrl = this
+    //this stores all the relevent static information about the student
+    //linkedin github....
+    // ctrl.prof = m.prop() // TODO: grab from makerpass
+    // ctrl.applied = m.prop()
+    // //
+
+
+    // ctrl.test = function() {
+    //   alert('Button Works')
+    // };
+    // ctrl.initialize = function () {
+    //   console.log("Getting jobs...")
+    //   //GET User job applications from database
+    //   m.request({ method: 'GET', url: 'DATABASE API ROUTE' }).then(ctrl.applied)
+    // }
+
+    // ctrl.update = function () {
+    //   console.log("...")
+    //   //Update User job applications from database
+    //   m.request({ method: 'POST', url: 'DATABASE API ROUTE' }).then(ctrl.initialize)
+    // }
+
+    // ctrl.create = function () {
+    //   console.log("...")
+    //   //Create User job applications from database
+    //   m.request({ method: 'POST', url: 'DATABASE API ROUTE' }).then(ctrl.initialize)
+    // }
 }
 
 exports.view = function (ctrl) {
-  var modelData = StudentApps.all()
-  return m('.container', [
-  	m('.row'),
-  	m('.row', [
-	    m.component(StudentInfo, { studentInfo: modelData['studentInfo'] } ),
-	  	m.component(StudentJobs, { apps: modelData['apps'], studentInfo: modelData['studentInfo'] } ),
-    ]),
-    m.component(NewApp)
-  ]) 
+  return m('.profile', [
+    m('h1', "Hello " + ctrl),
+    m('h3', "Current Applications"),
+    m('button')
+    //here is where we grab the jobs from the database
+    //
+    //Phone Screen
+    //Technical Screen
+    //Coding Challenge
+    //On-site interview
+  ])
 }
+
+// function jobs (ctrl, prof) {
+//   return m('.jobs', [
+//     m('h4', 'asd')
+//     //more stuff
+//   ])
+// }
+
+
+
+  //TODO steal gilberts pluralize function from petshop
+
+  // function pluralize (word, count) {
+  //   if (count !== 1) { word = word + 's' }
+  //   return count + ' ' + word
+  // }
+
+  // m.mount(document.getElementById('app'), StudentProfile)
