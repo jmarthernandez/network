0b8c8140d7eaadbab8922ee954b1400e0d442f4d
Justin Hernandez <jmarthernandez@gmail.com>
2015-06-19 22:45:33 -0500

    Moved forms into components/forms/

--- client/app-bundle/components/forms/newApp/newApp.js
+++ client/app-bundle/components/forms/newApp/newApp.js
@@ -2,109 +2,64 @@ var m = require('mithril');
 
 exports.controller = function () {
   var ctrl = this;
-  var newAppForm = {
-    phase: 1,
-    date_applied: '',
-    contact_id: 1,
-    app_method: '',
-    user_id: '',
-    active: true,
-    // location: '',
-    title_id: '',
-    company_id: ''
-  };
-
-    m.request({
-      method: 'GET',
-      url: "/me/"
-    }).then(function(req){
-      ctrl.newAppForm = m.prop(newAppForm);
-      ctrl.newAppForm().user_id = req.user.uid 
-      console.log(ctrl.newAppForm(), 'newAppForm');
-    });
-
-  ctrl.postApp = function(e, data) {
-    e.preventDefault()
-
-    m.request({
-        method: 'POST',
-        url: "/API/applications/",
-        data: ctrl.newAppForm,
-        }).then(function(data) {
-          console.log(data, 'postApp');
-          // m.redraw.strategy('all')
-        })
-  };
-
-  ctrl.getCompany = function(e, data) {
-    e.preventDefault()
-    m.request({
-      method: 'GET',
-      url: "/API/companies/",
-    }).then(function(companies){
-      console.log(companies)
-    });
-  };
-
-};  
-
-var binds = function(data) {
-  return {onchange: function(e) {
-    data[e.target.name] = e.target.value;
-  }};
-};
-
-
+}
 
 exports.view = function (ctrl) {
-  return m('.row', binds(ctrl.newAppForm()), [
+  return m('.row', [
     m('.row', [
-      m('h3.center-align', 'Add Application')
+      m('h3.center-align', 'Initial Application')
     ]),
     m('form.col.s12', [
       m('.row', [
-      m('.input-field.col.s12.m6', [
-          //Should have a limit of text
-          m('input#first_name.validate[type=text][placeholder="company"][name=company_id]', {onclick: ctrl.getJob}),
-          m('label', "company")
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder=Company]'),
+          m('label[for=first_name]', "Company")
         ]),
-      // m('.input-field.col.s12.m6', [
-      //     //Should auto complete for common jobs
-      //     m('input#first_name.validate[type=text][placeholder="active"][name=active]', {value: ctrl.newAppForm().active}, console.log(ctrl.newAppForm())),
-      //     m('label', "active")
-      //   ]),
-        // m('.input-field.col.s12.m6', [
-        //   //Should have a limit of text
-        //   m('input#first_name.validate[type=text][placeholder="phase"][name=phase]', {value: ctrl.newAppForm().phase}),
-        //   m('label', "phase")
-        // ]),
-     
         m('.input-field.col.s12.m6', [
           //Should auto complete for common companies
-          m('input#first_name.validate[type=text][placeholder=title][name=title_id]', {value: ctrl.newAppForm().title_id}),
-          m('label', "title")
-        ]),
-           m('.input-field.col.s12.m6', [
-          //Should have a limit of text
-          m('input#first_name.validate[type=text][placeholder="application method"][name=app_method]', {value: ctrl.newAppForm().app_method}),
-          m('label', "application method")
+          m('input#first_name.validate[type=text][placeholder=Location]'),
+          m('label[for=first_name]', "Location")
+        ])
+      ]),
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder="Company Website"]'),
+          m('label[for=first_name]', "Company Website")
         ]),
         m('.input-field.col.s12.m6', [
           //Should auto complete for common jobs
-          m('input#first_name.datepicker[type=date][placeholder=""][name=date_applied]', {value: ctrl.newAppForm().date_applied}),
-          m('label', "")
-        ]),
-        // m('.input-field.col.s12.m6', [
-        //   //Should auto complete for common jobs
-        //   m('input#first_name.validate[type=text][placeholder="contact"][name=contact_id]', {value: ctrl.newAppForm().contact_id}),
-        //   m('label', "contact")
-        // ]),
+          m('input#first_name.validate[type=text][placeholder="Job Title"]'),
+          m('label[for=first_name]', "Job Title")
+        ])
       ]),
-      m('.row.center-align', [
-        // m('button.btn.waves-effect.waves-light[type=button]', 'Submit', {onclick: function() {postApp}},
-        m('button.btn.waves-effect.waves-light[type=button]', { onclick: ctrl.postApp }, 'Submit', [
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          //Should have a limit of text
+          m('input#first_name.validate[type=text][placeholder="How it Became a Lead"]'),
+          m('label[for=first_name]', "How it Became a Lead")
+        ]),
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder=Application Method]'),
+          //Should autocomplete for common methods
+          m('label[for=first_name]', "Application Method")
+        ])
+      ]),
+      m('.row', [
+        m('.input-field.col.s12.m6', [
+          //Should have a limit of text
+          m('input#first_name.datepicker[type=date][placeholder="Date Applied"]'),
+          m('label[for=first_name]', "Date Applied")
+        ]),
+        m('.input-field.col.s12.m6', [
+          m('input#first_name.validate[type=text][placeholder=Application Method]'),
+          //Should autocomplete for common methods
+          m('label[for=first_name]', "Application Method")
+        ])
+      ]),
+      m('.row', [
+        m('button.btn.waves-effect.waves-light', 'Submit',[
           //POST to database
-          // m('i.mdi-content-send.right')a
+          m('i.mdi-content-send.right')
         ])
       ])
     ])
