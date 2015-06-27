149e123c496f99c504c1ee0cc814a716c9cc3f0b
sbraunstein <sbraunstein12@gmail.com>
2015-06-27 17:25:33 -0500

    working view model

---
+++ client/app-bundle/models/Interview.js
@@ -0,0 +1,47 @@
+var m = require('mithril');
+
+
+var Interview = module.exports = {
+
+	vm: function (attrs) {
+		console.log('attrs',attrs);
+		attrs = attrs || ''
+
+			console.log('typeof attrs',typeof attrs);
+
+		return {
+			// id: attrs.id,
+			contacts_id: m.prop(''),
+			role: m.prop(''),
+			scheduled_date: m.prop(''),
+			occured_date: m.prop(''),
+			follow_up: m.prop(''),
+			quality: m.prop(''),
+			preparedness: m.prop('')
+
+
+			// scheduled_date: m.prop(attrs.scheduled_date || new Date()),
+			// quality: m.prop(attrs.quality || 3)
+		}
+	},
+
+	fetchInt: function (req) {
+		m.request({
+			methods: 'GET',
+			url: '/API/interviews/',
+
+		}).then(console.log('Interviews GET request'), req);
+	},
+
+	postInterview: function () {
+		m.request({
+			method: 'POST',
+			url: '/API/interviews',
+			data:Interview.vm(),
+		}).then(console.log('Interview POST'))
+	},
+	all: function() {
+		return Interview.vm()
+  }
+
+};
