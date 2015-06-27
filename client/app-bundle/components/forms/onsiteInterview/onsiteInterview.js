2d0a931a267ba767c7965ed6153a18e707a58a61
sbraunstein <sbraunstein12@gmail.com>
2015-06-27 17:00:33 -0500

    view-Model working

--- client/app-bundle/components/forms/onsiteInterview/onsiteInterview.js
+++ client/app-bundle/components/forms/onsiteInterview/onsiteInterview.js
@@ -1,15 +1,28 @@
 var m         = require('mithril');
-var AllInts   = require('../../../models/allInts.model.js');
+var Interview   = require('../../../models/Interview.js');
 
 exports.controller = function () {
 	var ctrl = this;
-  AllInts.fetchInt();
-  console.log()
+  // Interview.fetchInt();
+  ctrl.interview = Interview.vm();
+  console.log(Interview.vm(), "int vm")
+
+  ctrl.submit = function () {
+
+    Interview.postInterview();
+    // Interview.create( ctrl.interview ).then(function() {
+    //   console.log('we are submitting')
+    //   // Reset VM (or something)
+    //   ctrl.interview = Interview.vm()
+    // })
+  }
 }
 
 exports.view = function (ctrl) {
-  var modelData = AllInts.all();
-  console.log(AllInts);
+  var modelData = Interview.all();
+  // console.log('ha',ctrl.interview.contacts_id(data))
+
+  // ctrl.interview.contacts_id()
   return m('.row', [
     m('.row', [
       m('h3.center-align', 'On-site Interview')
@@ -21,16 +34,22 @@ exports.view = function (ctrl) {
       m('.row',[
         m('.input-field.col.s12.m4', [
           //Should have a limit of text
-          m('input#first_name.validate[type=text][placeholder="Name"][name=contact_id]', {value: AllInts.contacts_id}),
+          m('input.validate[type=text][placeholder="Name"][name=contacts_id]',{
+            value: ctrl.interview.contacts_id(),
+            onchange: m.withAttr('value', ctrl.interview.contacts_id)
+          }),
           m('label', "Name")
         ]),
         m('.input-field.col.s12.m4', [
-          m('input#first_name.validate[type=text][placeholder="Role"]'),
+          m('input.validate[type=text][placeholder="Role"][name=role]', {
+            value: ctrl.interview.role(),
+            onchange: m.withAttr('value', ctrl.interview.role)
+          }),
           //Should autocomplete for common methods
           m('label[for=first_name]', "Role")
         ]),
         m('.input-field.col.s12.m4', [
-          m('input#first_name.validate[type=email][placeholder="Email"]'),
+          m('input.validate[type=email][placeholder="Email"]', console.log('view', JSON.stringify(ctrl.interview))),
           //Should autocomplete for common methods
           m('label[for=first_name]', "Email")
         ])
@@ -41,11 +60,18 @@ exports.view = function (ctrl) {
       m('.row', [
         m('.input-field.col.s12.m6', [
           //Should have a limit of text
-          m('input#first_name.datepicker[type=date][placeholder="Date Applied"]'),
+          m('input.datepicker[type=date][placeholder="scheduled_date"]', {
+            value: ctrl.interview.scheduled_date(),
+            onchange: m.withAttr('value', ctrl.interview.scheduled_date),
+          }),
           m('label[for=first_name]', "Scheduled For")
         ]),
         m('.input-field.col.s12.m6', [
-          m('input#first_name.datepicker[type=date][placeholder="Date Applied"]'),
+          m('input.datepicker[type=date][placeholder="occured_date"]',{
+            value: ctrl.interview.occured_date(),
+            onchange: m.withAttr('value', ctrl.interview.occured_date),
+          }),
+
           m('label[for=first_name]', "Completed On")
         ])
       ]),
@@ -53,10 +79,13 @@ exports.view = function (ctrl) {
       //   <input type="range" id="test5" min="0" max="100" />
       // </p>
       m('p.range-field', 'How did it go?',[
-        m('input#test5[type=range][min=0][max=5]')
+        m('input#test5[type=range][min=0][max=5]', {
+          value: ctrl.interview.preparedness(),
+          onchange: m.withAttr('value', ctrl.interview.preparedness),
+        })
       ]),
       m('.row', [
-        m('button.btn.waves-effect.waves-light', 'Submit',[
+        m('button.btn.waves-effect.waves-light', 'Submit', {onclick: ctrl.submit},[
           //POST to database
           m('i.mdi-content-send.right')
         ])
