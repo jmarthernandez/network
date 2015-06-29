cd26ffc4f0b8c50c0f5f5f0bc663dca58e12fe25
Dan Corman <dan.corman@gmail.com>
2015-06-29 15:58:49 -0500

    updating routing for school

--- server/API/schools-api.js
+++ server/API/schools-api.js
@@ -4,13 +4,13 @@ var express = require('express')
 var router = module.exports = express.Router();
 
 	//endpoint which retreives all schools
-	router.get('/API/School', function(req, res){
+	router.get('/', function(req, res){
 		School.retrieveAll(function(x){res.send({School: x});
     });
   });
 
 	//endpoint which posts a new school
-	router.post('/API/School', function(req, res){
+	router.post('/', function(req, res){
 		if (!req.body) return res.sendStatus(400);
 		var newValues = School.updateOrCreate(req.body);
 		res.send(req.body);
