64e55c0ecff2c4fd1e5e1b4246fae14ba5d4ba05
Dan Corman <dan.corman@gmail.com>
2015-07-01 20:26:08 -0500

    removed erroneous objects

--- client/app-bundle/models/Fuzzy.js
+++ client/app-bundle/models/Fuzzy.js
@@ -2,18 +2,6 @@ var m = require('mithril');
 
 
 var Fuzzy = module.exports = {
-    tempValue: null,
-
-    vm: function (attrs) {
-    attrs = attrs || '';
-
-    return {
-      search: m.prop(''),
-    };
-  },
-
-
-
    search: function(table , newQuery){
     return m.request({ method: 'GET', url: 'api/fuzzy/' + table + '/' + newQuery})
   }
