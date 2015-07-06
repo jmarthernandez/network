68e583634a0818edf0ecd7e0f84839cf7d819754
Dan Corman <dan.corman@gmail.com>
2015-07-06 16:09:24 -0500

    added endpoint for graphs

--- server/models/Interview.js
+++ server/models/Interview.js
@@ -2,3 +2,13 @@ var db = require('../db.js');
 var Promise = require('bluebird');
 var General = require('../lib/General.js');
 var Interviews = module.exports = General.access('interviews');
+
+
+
+module.exports.megaJoin = function(){
+	return db.select('users.*','interviews.*','applications.*').from('interviews')
+	.join('applications', function() {
+        this.on('applications.id', '=', 'interviews.app_id')})
+      .join('users',function() {
+        this.on('users.uid', '=', 'applications.user_id')})
+}
