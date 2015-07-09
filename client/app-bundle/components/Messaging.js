b26ce15d67cf8b255b3f49c2c3315d582e649913
Justin Hernandez <jmarthernandez@gmail.com>
2015-07-09 16:52:33 -0500

    Move chat to bottom modal

--- client/app-bundle/components/Messaging.js
+++ client/app-bundle/components/Messaging.js
@@ -1,4 +1,5 @@
 var m = require('mithril');
+var materialize = require('../../lib/materialize.js');
 
 //model
 var Message   = require('../models/Message.js');
@@ -44,46 +45,53 @@ exports.view = function (ctrl, options) {
   };
   ctrl.allMessages = options.messages;
   ctrl.message.sender_uid = options.studentInfo.uid;
-  return m( '.row', [
-    m('h1.center-align', 'Messages'),
-    m('form.col.s12', { onsubmit: ctrl.submit }, [
-      m('ul.collection', [
-        options.messages.filter(ctrl.filter).map(function(message){
-          return m('li.collection-item avatar', [
-            m('p', 'From: ' + message.sender_name),
-            m('p', 'To: ' + message.receiver_name),
-            m('span.title', message.body),
-          ])
-        })
-      ]),
-      m('ul', [
-        options.users.map(function(user){
-          if(user.uid !== options.studentInfo.uid){
-            return m('li', [
-              m('a', {
-                value: user.uid,
-                me: options.studentInfo.name,
-                onclick: ctrl.filterMessages
-              }, user.name),
+  
+  return m('section', [
+    m('a.btn.modal-trigger', { href: '#chat-modal', config: materialize.modalClick }, 'Send a Message'),
+    m('.modal.bottom-sheet#chat-modal', [
+      m('.modal-content', [
+        m('h4', ctrl.selectedUser),
+        m('p', 'asdf'),
+        m('form.col.s12', { onsubmit: ctrl.submit }, [
+          m('ul', [
+            options.messages.filter(ctrl.filter).map(function(message){
+              return m('li.collection-item avatar', [
+                m('p', 'From: ' + message.sender_name),
+                m('p', 'To: ' + message.receiver_name),
+                m('span', message.body),
+              ])
+            })
+          ]),
+          m('ul', [
+            options.users.map(function(user){
+              if(user.uid !== options.studentInfo.uid){
+                return m('li', [
+                  m('a', {
+                    value: user.uid,
+                    me: options.studentInfo.name,
+                    onclick: ctrl.filterMessages
+                  }, user.name),
+                ])
+              }
+            })
+          ]),
+          m('.row', [        
+            m('.row.input-field.col.l6.m6.s12', [
+              m('i.mdi-editor-mode-edit.prefix'),
+              m('textarea#icon_prefix2.materialize-textarea', {
+                value: ctrl.message.body(),
+                onchange: m.withAttr('value', ctrl.message.body)
+              }),
+              m('label[for=icon_prefix2]', 'Message to : ' + ctrl.selectedUser)
             ])
-          }
-        })
-      ]),
-      m('.row', [        
-        m('.row.input-field.col.l6.m6.s12', [
-          m('i.mdi-editor-mode-edit.prefix'),
-          m('textarea#icon_prefix2.materialize-textarea', {
-            value: ctrl.message.body(),
-            onchange: m.withAttr('value', ctrl.message.body)
-          }),
-          m('label[for=icon_prefix2]', 'Message to : ' + ctrl.selectedUser)
-        ])
-      ]),
-      m('.div.center-align', [
-        m('button.btn.waves-effect.waves-light', 'Send Message',[
-          m('i.mdi-content-send.right')
+          ]),
+          m('.div.center-align', [
+            m('button.btn.waves-effect.waves-light', 'Send Message',[
+              m('i.mdi-content-send.right')
+            ])
+          ])
         ])
       ])
     ])
-  ]);
+  ])
 };
