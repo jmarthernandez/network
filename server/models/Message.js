e094df62d5c3dddfd852042eef970841a37771e2
Dan Corman <dan.corman@gmail.com>
2015-07-10 19:17:43 -0500

    recent messages at top

--- server/models/Message.js
+++ server/models/Message.js
@@ -6,13 +6,11 @@ var Messages = module.exports = General.access('messages');
 
 module.exports.retrieveOne = function (uid) {
 
-  return db.select('messages.*', 'sender.name AS sender_name', 'sender.uid','sender.status','sender.avatar_url AS sender_url', 'receiver.name AS receiver_name').from('messages').where({'sender_uid': uid}).orWhere({'receiver_uid':uid})
-  .join('users AS sender', 'messages.sender_uid', 'sender.uid')
-  .join('users AS receiver', 'messages.receiver_uid', 'receiver.uid').orderBy('created_at', 'asc')
-  .then(function(rows){
-   return rows;
-  })
-};
-
-
+  return db.select('messages.*', 'sender.name AS sender_name', 'sender.uid','sender.status','sender.avatar_url AS sender_url', 'receiver.name AS receiver_name')
+  	.from('messages')
+  	.where({'sender_uid': uid})
+  	.orWhere({'receiver_uid': uid})
+  	.join('users AS sender', 'messages.sender_uid', 'sender.uid')
+  	.join('users AS receiver', 'messages.receiver_uid', 'receiver.uid').orderBy('updated_at','DESC')
+}
 
