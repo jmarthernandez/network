5d4ff550f1cd8761238e13c0a3acdadbd91e0159
RobertSnodgrass <robert.snodgrass@gmail.com>
2015-07-11 11:42:13 -0500

    Added drop down to Outcomes view

--- client/app-bundle/components/OutcomesJobs.js
+++ client/app-bundle/components/OutcomesJobs.js
@@ -29,118 +29,138 @@ exports.view = function(ctrl, options){
         ]),
       ]), //End Tabs UL
     ]), //End Div.Col.S12 
-  ]), //Ends Row Just Under Return M
+  ]), //Ends Row Just Under Head
 
   //Begin Active Applications (Phase 1)
   m('div#activeApps', [
     m('h5.center-align', 'Pending Applications'),
-    m('ul.collapsible#shorten[data-collapsible=accordion]', { config: materialize.makeCollapsible}, [
+    m('ul.collapsible#shorten[data-collapsible=accordion]', [
       fuzzyName.apps['1'].map(function(app){
         return m('li', [
-          m('div.collapsible-header', { class: 'green lighten-' + (5 -  app.count) }, [
-            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title)
+          m('div.collapsible-header', [
+            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title), 
+            m('a', [
+              m('i.mdi-action-view-headline.dropdown-button[href=#][data-activates=dropdown1]', {config: materialize.dropDowns})
+            ])
           ]),
-          m('.collapsible-body.center-align', [
-            m("br"),
-            m('a.waves-effect.waves-light.btn[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
-            m('a.waves-effect.waves-light.btn[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
-            m('a.waves-effect.waves-light.btn[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
-            m('a.waves-effect.waves-light.btn[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
-          ])
-        ])
+          m('ul#dropdown1', {class:'dropdown-content'}, [
+            m('li', [
+              m('a.waves-effect.waves-light[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
+              m('a.waves-effect.waves-light[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
+              m('a.waves-effect.waves-light[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
+              m('a.waves-effect.waves-light[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
+            ])
+          ]),
+        ]) //End Return M LI
       })
-    ]), 
+    ]), //End UL Collapsible
   ]), //End Active Applications (Phase I)
 
   //Begin Phone Interviews (Phase 2)
   m('div#events', [
-    m('div#testSmall.m4.s12', [
+    m('div#eventSmall.m4.s12', [
     m('h5.center-align', 'Phone Interviews'),
-    m('ul.collapsible[data-collapsible=accordion]', { config: materialize.makeCollapsible}, [
+    m('ul.collapsible[data-collapsible=accordion]', [
       fuzzyName.apps['2'].map(function(app){
         return m('li', [
           m('div.collapsible-header', { class: 'green lighten-' + (5 -  app.count) }, [
-            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title)
+            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title),
+            m('a', [
+              m('i.mdi-action-view-headline.dropdown-button[href=#][data-activates=dropdown2]', {config: materialize.dropDowns})
+            ]),
           ]),
-          m('.collapsible-body.center-align', [
-            m("br"),
-            m('a.waves-effect.waves-light.btn[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
-            m('a.waves-effect.waves-light.btn[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview'),
-            m('a.waves-effect.waves-light.btn[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
-            m('a.waves-effect.waves-light.btn[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen')
-          ])
-        ])
+          m('ul#dropdown2', {class:'dropdown-content'}, [
+            m('li', [
+              m('a.waves-effect.waves-light[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
+              m('a.waves-effect.waves-light[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
+              m('a.waves-effect.waves-light[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
+              m('a.waves-effect.waves-light[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
+            ])
+          ]),
+        ]) //End Return M LI
       })
-    ]),
-    ]),  //End #testSmall
+    ]), //End UL Collapsible
+    ]), //End #eventSmall
   //]), //End Phone Interviews (Phase 2)
 
   //Begin Onsites (Phase 3)
   //m('div#gg', [
-    m('div#testSmall.m4.s12', [
+    m('div#eventSmall.m4.s12', [
     m('h5.center-align', 'Challenges and Tech Interviews'),
     //m('h5.center-align', 'Coding Challenges/Tech Interviews: ' +  fuzzyName.apps[3].length),
-    m('ul.collapsible[data-collapsible=accordion]', { config: materialize.makeCollapsible}, [
+    m('ul.collapsible[data-collapsible=accordion]', [
       fuzzyName.apps['3'].map(function(app){
         return m('li', [
           m('div.collapsible-header', { class: 'green lighten-' + (5 -  app.count) }, [
-            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title)
+            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title),
+            m('a', [
+              m('i.mdi-action-view-headline.dropdown-button[href=#][data-activates=dropdown3]', {config: materialize.dropDowns})
+            ]),
           ]),
-          m('.collapsible-body.center-align', [
-            m("br"),
-            m('a.waves-effect.waves-light.btn[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
-            m('a.waves-effect.waves-light.btn[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
-            m('a.waves-effect.waves-light.btn[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
-            m('a.waves-effect.waves-light.btn[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
-          ])
-        ])
+          m('ul#dropdown3', {class:'dropdown-content'}, [
+            m('li', [
+              m('a.waves-effect.waves-light[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
+              m('a.waves-effect.waves-light[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
+              m('a.waves-effect.waves-light[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
+              m('a.waves-effect.waves-light[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
+            ])
+          ]),
+        ]) //End Return M LI
       })
-    ]),
-    ]),  //End #testSmall
+    ]), //End UL Collapsible
+    ]), //End #eventSmall
   //]), //End Interviews (Phase 3)
 
   //Begin Onsites (Phase 4)
   //m('div#ggg', [
-    m('div#testSmall.m4.s12', [
+    m('div#eventSmall.m4.s12', [
     m('h5.center-align', 'Onsite Interviews'),
-    m('ul.collapsible[data-collapsible=accordion]', { config: materialize.makeCollapsible}, [
+    m('ul.collapsible[data-collapsible=accordion]', [
       fuzzyName.apps['4'].map(function(app){
         return m('li', [
           m('div.collapsible-header', { class: 'green lighten-' + (5 -  app.count) }, [
-            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title)
+            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title),
+            m('a', [
+              m('i.mdi-action-view-headline.dropdown-button[href=#][data-activates=dropdown4]', {config: materialize.dropDowns})
+            ]),
           ]),
-          m('.collapsible-body.center-align', [
-            m("br"),
-            m('a.waves-effect.waves-light.btn[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
-            m('a.waves-effect.waves-light.btn[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
-            m('a.waves-effect.waves-light.btn[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
-            m('a.waves-effect.waves-light.btn[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
-          ])
-        ])
+          m('ul#dropdown4', {class:'dropdown-content'}, [
+            m('li', [
+              m('a.waves-effect.waves-light[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
+              m('a.waves-effect.waves-light[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
+              m('a.waves-effect.waves-light[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
+              m('a.waves-effect.waves-light[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
+            ])
+          ]),
+        ]) //End Return M LI
       })
-    ]),
-    ]),  //End #testSmall
+    ]), //End UL Collapsible
+    ]), //End #eventSmall
   ]), //End Onsites (Phase 4)  //End#Events
 
   //Begin Offers (Phase 5)
   m('div#offers', [
     m('h5.center-align', 'Offers'),
-    m('ul.collapsible[data-collapsible=accordion]', { config: materialize.makeCollapsible}, [
+    m('ul.collapsible[data-collapsible=accordion]', [
       fuzzyName.apps['5'].map(function(app){
         return m('li', [
           m('div.collapsible-header', { class: 'green lighten-' + (5 -  app.count) }, [
-            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title)
+            m('a.app-link[href=/appdetail/' + app.app_id + ']', { config: m.route }, m('img[src=' + app.avatar_url + '].circle.app'), app.name + ' ' + app.company_name + ': ' + app.title),
+            m('a', [
+              m('i.mdi-action-view-headline.dropdown-button[href=#][data-activates=dropdown5]', {config: materialize.dropDowns})
+            ]),
           ]),
-          m('.collapsible-body.center-align', [
-            m("br"),
-            m('a.waves-effect.waves-light.btn[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
-            m('a.waves-effect.waves-light.btn[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
-            m('a.waves-effect.waves-light.btn[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
-            m('a.waves-effect.waves-light.btn[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
-          ])
-        ])
+          m('ul#dropdown5', {class:'dropdown-content'}, [
+            m('li', [
+              m('a.waves-effect.waves-light[href=/phonescreen/' + app.app_id + ']', { config: m.route }, 'Phone Screen'),
+              m('a.waves-effect.waves-light[href=/technicalscreen/' + app.app_id + ']', { config: m.route }, 'Technical Screen'),
+              m('a.waves-effect.waves-light[href=/codingchallenge/' + app.app_id + ']', { config: m.route }, 'Coding Challenge'),
+              m('a.waves-effect.waves-light[href=/onsiteinterview/' + app.app_id + ']', { config: m.route }, 'Onsite Interview')
+            ])
+          ]),
+        ]) //End Return M LI
       })
-    ]),//ends ul collaps
+    ]), //End UL Collapsible
   ]),
     //End Offers (Phase 5)
 m.component(Fuzzy, {
@@ -154,5 +174,5 @@ m.component(Fuzzy, {
         },
         route: m('a.waves-effect.waves-light.btn', 'Lookup Student')
       }),
-  ]); //Ends Return M .col.m9.s12'
+  ]); //Ends Return M.col.m9.s12
 };
--- client/lib/materialize.js
+++ client/lib/materialize.js
@@ -5,24 +5,26 @@ exports.makeCollapsible = function(elem, isInitialized){
 		accordion: false
 	});
 
-	//config: materialize.makeCollapsible
+  //Currently, this method is not actively called; functionality
+  //was present in Student/Outcomes views for list of applications,
+  //but this was changed to drop down buttons late in project life
 };
 
 exports.pickDates = function(elem, isInitialized){
 	if (isInitialized) return;
 
-
   $(elem).pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year
+    selectMonths: true, // Creates dropdown to control month
+    selectYears: 15, // Creates dropdown of 15 years to control year
 
     dateFormat: 'yyyy-mm-dd',
-  	
 	  format: 'mmmm dd, yyyy',
 	  formatSubmit: 'yyyy-mm-dd',
 	  hiddenPrefix: 'prefix__',
 	  hiddenSuffix: '__suffix',
 
+	  //The onSet section is needed to prevent the modal from closing when
+		//clicking left/right buttons to cycle through months
 		onSet: function (date) {
 			console.log(date.select)
 			if(date.select === undefined) return;
@@ -30,32 +32,28 @@ exports.pickDates = function(elem, isInitialized){
 		},
 
 		onClose: function(){
-	        $(document.activeElement).blur()
-	    }
-    //{class: 'datepicker', config: materialize.pickDates},
+	    $(document.activeElement).blur()
+	  }
   });
 };
 
 exports.tabInit = function(elem, isInitialized){
 	if (isInitialized) return;
 
-  $(elem).tabs();  //.ul.tabs
+  $(elem).tabs(); 
 };
-
-
 
 exports.modalClick = function(elem, isInitialized){
 	if (isInitialized) return;
 
   $(elem).leanModal({  //.modal-trigger
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
-      in_duration: 300, // Transition in duration
-      out_duration: 200, // Transition out duration
-      // ready: function() { alert('Ready'); }, // Callback for Modal open
-      // complete: function() { alert('Closed'); } // Callback for Modal close
-    }
-  );
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    in_duration: 300, // Transition in duration
+    out_duration: 200, // Transition out duration
+    // ready: function() { alert('Ready'); }, // Callback for Modal open
+    // complete: function() { alert('Closed'); } // Callback for Modal close
+  });
 };
 
 exports.navDrop = function(elem, isInitialized){
@@ -68,30 +66,24 @@ exports.dropDowns = function (elem, isInitialized){
 	if (isInitialized) return;
 
 	$(elem).dropdown({
-      inDuration: 300,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: false, // Activate on hover
-      gutter: 10, // Spacing from edge
-      belowOrigin: true // Displays dropdown below the button
-    }
-  );
+    inDuration: 300,
+    outDuration: 225,
+    constrain_width: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 10, // Spacing from edge
+    belowOrigin: true // Displays dropdown below the button
+  });
 }
 
 exports.carosel = function(elem, isInitialized){
 	if (isInitialized) return;
+
   $(elem).slider({full_width: true});
 }
 
 exports.fullScreenSlider = function(elem, isInitialized){
 	if (isInitialized) return;
 
-	$(elem).slider({full_width: true}); //.slider
+	$(elem).slider({full_width: true});
 }
-
-
-
-
-
-
 
